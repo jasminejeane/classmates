b28bc110423b78338830047e0313c03038244d2a
jasminejeane <jasminejeane@gmail.com>
2018-01-07 22:52:29 -0800

    firebase and imgs added

---
+++ classmates-2002/src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+var config = {
+   apiKey: "AIzaSyCrN4TXtIbUH2wyGO52IqMmIjfM5xdB7Dw",
+   authDomain: "classmates-deca7.firebaseapp.com",
+   databaseURL: "https://classmates-deca7.firebaseio.com",
+   projectId: "classmates-deca7",
+   storageBucket: "",
+   messagingSenderId: "210941239171"
+ };
+ firebase.initializeApp(config);
+
+ export default 'firebase';
