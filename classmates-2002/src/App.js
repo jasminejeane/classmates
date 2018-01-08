b8ad682dc5dfef29050d1e8198dab43548cf12a2
jasminejeane <jasminejeane@gmail.com>
2018-01-07 23:36:56 -0800

    form outline ready

--- classmates-2002/src/App.js
+++ classmates-2002/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+//
 class App extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       currentItem:
+//     }
+//   }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header>
+            <div className='wrapper'>
+              <h1>Find Friends</h1>
+
+            </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container'>
+          <section className='add-item'>
+              <form>
+                <input type="text" name="classmateName" placeholder="Classmates Name" />
+                <input type="phone" name="contact" placeholder="phone number" />
+                <input type="email" name="contactInfo" placeholder="email" />
+                <input type="url" name="url" placeholder="social media link" />
+                <input type="text" name="moreinfo" placeholder="Other info" />
+                <input type="text" name="yourName" placeholder="What's your name?" />
+              <button>Add Item</button>
+              </form>
+          </section>
+          <section className='display-item'>
+            <div className='wrapper'>
+              <ul>
+              </ul>
+            </div>
+          </section>
+        </div>
       </div>
     );
   }
